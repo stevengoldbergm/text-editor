b497ecae6087037ddcc18729d828de5c8dca5093
SG_Demi <stevengoldbergm@gmail.com>
2022-10-13 17:23:33 -0500

    Updated the name of the app for the manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,8 +32,8 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Text Editor',
-        short_name: 'txt',
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
         description: 'Write lines of whatever!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
